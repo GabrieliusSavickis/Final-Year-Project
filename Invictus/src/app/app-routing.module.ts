f054522685e97eb3a935ed2a40b5dca6ebf6bae0
GabrieliusSavickis <gabrrielius@gmail.com>
2023-12-11 18:25:54 +0000

    Added tabs, need to fix the routing and display the tabs on the bottom of the screen

--- Invictus/src/app/app-routing.module.ts
+++ Invictus/src/app/app-routing.module.ts
@@ -1,45 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { TabsComponent } from './tabs/tabs.component';
 
 const routes: Routes = [
+  // Upon deployment redirect user to the login page
   {
-    path: 'tabs',
-    component: TabsComponent, // Create a new component to hold the ion-tabs
-    children: [
-      {
-        path: 'home',
-        loadChildren: () => import('./Pages/home/home.module').then(m => m.HomePageModule)
-      },
-      {
-        path: '',
-        redirectTo: 'login',
-        pathMatch: 'full'
-      },
-      {
-        path: 'exercises',
-        loadChildren: () => import('./Pages/exercises/exercises.module').then(m => m.ExercisesPageModule)
-      },
-      {
-        path: 'trainer',
-        loadChildren: () => import('./Pages/trainer/trainer.module').then(m => m.TrainerPageModule)
-      },
-      {
-        path: 'profile',
-        loadChildren: () => import('./Pages/profile/profile.module').then(m => m.ProfilePageModule)
-      },
-      {
-        path: '',
-        redirectTo: '/tabs/home',
-        pathMatch: 'full'
-      }
-    ]
+    path: '',
+    loadChildren: () => import('./Pages/login/login.module').then( m => m.LoginPageModule)
   },
   {
-
-    path: 'login',
-    loadChildren: () => import('./Pages/login/login.module').then(m => m.LoginPageModule)
-
+    path: 'home',
+    loadChildren: () => import('./Pages/home/home.module').then( m => m.HomePageModule)
   },
 ];
 
