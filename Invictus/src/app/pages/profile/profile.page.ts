e1e7b3192d63d0ac6fddbf6939458521cc825ee8
GabrieliusSavickis <gabrrielius@gmail.com>
2023-12-16 14:27:23 +0000

    Updated profile fields

--- Invictus/src/app/pages/profile/profile.page.ts
+++ Invictus/src/app/pages/profile/profile.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { AuthService, LogoutOptions } from '@auth0/auth0-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile-page',
@@ -8,12 +10,55 @@ import { UserService } from '../../services/user.service';
 })
 export class ProfilePageComponent implements OnInit {
   userProfile: any;
+  phone: string;
+  age: number;
+  gender: string;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, public auth: AuthService, private httpClient: HttpClient) {
+    this.phone = '';
+    this.age = 0;
+    this.gender = '';
+  }
+
+  logout() {
+    this.auth.logout({ returnTo: `${window.location.origin}/login` } as LogoutOptions);
+  }
 
   ngOnInit() {
     this.userService.userProfile$.subscribe(profile => {
       this.userProfile = profile;
+
+      // Fetch additional profile info from MongoDB using the email
+    this.fetchProfileData(this.userProfile.email);
     });
   }
+
+  fetchProfileData(email: string) {
+    this.httpClient.get(`http://localhost:3000/profile/${email}`).subscribe((data: any) => {
+      this.phone = data.phone;
+      this.age = data.age;
+      this.gender = data.gender;
+      // Handle other properties if necessary
+    });
+  }
+
+  saveProfile() {
+    const profileData = {
+      email: this.userProfile.email, // Make sure to send the email as an identifier
+      phone: this.phone,
+      age: this.age,
+      gender: this.gender
+    };
+
+    this.httpClient.post('http://localhost:3000/profile/update', profileData).subscribe((response) => {
+      console.log('Profile updated:', response);
+      // Add any additional logic for a successful update
+    }, (error) => {
+      console.error('Error updating profile:', error);
+      // Add error handling logic
+    });
 }
+
+
+}
+
