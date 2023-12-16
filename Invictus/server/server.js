817fefcbd20c505d8edce0c599526dd8091f9a27
GabrieliusSavickis <gabrrielius@gmail.com>
2023-12-16 14:26:32 +0000

    Added profile fields, not working on codespaces because it acts as a VM

--- Invictus/server/server.js
+++ Invictus/server/server.js
@@ -24,6 +24,10 @@ app.listen(port, () => {
 const userSchema = mongoose.Schema({
   height: Number,
   weight: Number,
+  email: String, // This is the email address of the user which we can uniquely identify them by
+  phone: String,
+  age: Number,
+  gender: String,
 }, {
   // Shows when changes are made to the data
   timestamps: true,
@@ -42,6 +46,26 @@ app.post('/tabs/trainer', async (req, res) => {
     res.send(user);
   } catch (error) {
     res.status(500).send('Error saving user data'); //trying to catch the error
+  }
+});
+
+// Get user data
+app.get('/tabs/profile/:email', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.params.email });
+    res.send(user);
+  } catch (error) {
+    res.status(500).send('Error retrieving user data');
+  }
+});
+
+// Update user data
+app.post('/tabs/profile/update', async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate({ email: req.body.email }, req.body, { new: true });
+    res.send(user);
+  } catch (error) {
+    res.status(500).send('Error updating user data');
   }
 });
 
