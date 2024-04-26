0d883617e2e17df80328c4a9cc2ec045bb4c8a21
Gabrielius <gabrrielius@gmail.com>
2024-04-26 10:23:07 +0100

    Added more exercise categories for future development

--- Invictus/src/app/pages/exercises/exercises-data.ts
+++ Invictus/src/app/pages/exercises/exercises-data.ts
@@ -10,6 +10,7 @@ export interface Exercise {
     expanded: boolean; // Flag to track card expansion
     imageUrl: string; 
     exercises: Exercise[];
+    
   }
   
   export const exerciseCategories: ExerciseCategory[] = [
@@ -75,4 +76,184 @@ export interface Exercise {
         },
       ],
     },
+    {
+      id: 'back',
+      name: 'Back',
+      expanded: false,
+      imageUrl: '../../../assets/icon/back.jpg',
+      exercises: [
+        {
+          id: 'pull-up',
+          name: 'Pull-Up',
+        },
+        {
+          id: 'deadlift',
+          name: 'Deadlift',
+        },
+        {
+          id: 'bent-over-row',
+          name: 'Bent Over Row',
+        },
+      ],
+    },
+    {
+      id: 'legs',
+      name: 'Legs',
+      expanded: false,
+      imageUrl: '../../../assets/icon/legs.jpg',
+      exercises: [
+        {
+          id: 'squat',
+          name: 'Squat',
+        },
+        {
+          id: 'leg-press',
+          name: 'Leg Press',
+        },
+        {
+          id: 'lunges',
+          name: 'Lunges',
+        },
+      ],
+    },
+    {
+      id: 'triceps',
+      name: 'Triceps',
+      expanded: false,
+      imageUrl: '../../../assets/icon/triceps.jpg',
+      exercises: [
+        {
+          id: 'tricep-dips',
+          name: 'Tricep Dips',
+        },
+        {
+          id: 'tricep-extension',
+          name: 'Tricep Extension',
+        },
+        {
+          id: 'close-grip-bench-press',
+          name: 'Close Grip Bench Press',
+        },
+      ],
+    },
+    {
+      id: 'core',
+      name: 'Core',
+      expanded: false,
+      imageUrl: '../../../assets/icon/core.jpg',
+      exercises: [
+        {
+          id: 'plank',
+          name: 'Plank',
+        },
+        {
+          id: 'crunches',
+          name: 'Crunches',
+        },
+        {
+          id: 'leg-raises',
+          name: 'Leg Raises',
+        },
+      ],
+    },
+    {
+      id: 'cardio',
+      name: 'Cardio',
+      expanded: false,
+      imageUrl: '../../../assets/icon/cardio.jpg',
+      exercises: [
+        {
+          id: 'running',
+          name: 'Running',
+        },
+        {
+          id: 'jump-rope',
+          name: 'Jump Rope',
+        },
+        {
+          id: 'cycling',
+          name: 'Cycling',
+        },
+      ],
+    },
+    {
+      id: 'forearms',
+      name: 'Forearms',
+      expanded: false,
+      imageUrl: '../../../assets/icon/forearms.jpg',
+      exercises: [
+        {
+          id: 'wrist-curl',
+          name: 'Wrist Curl',
+        },
+        {
+          id: 'reverse-wrist-curl',
+          name: 'Reverse Wrist Curl',
+        },
+        {
+          id: 'hammer-grip-curl',
+          name: 'Hammer Grip Curl',
+        },
+      ],
+    },
+    {
+      id: 'abdominals',
+      name: 'Abdominals',
+      expanded: false,
+      imageUrl: '../../../assets/icon/abdominals.jpg',
+      exercises: [
+        {
+          id: 'crunches',
+          name: 'Crunches',
+        },
+        {
+          id: 'leg-raises',
+          name: 'Leg Raises',
+        },
+        {
+          id: 'planks',
+          name: 'Planks',
+        },
+      ],
+    },
+    {
+      id: 'abductors',
+      name: 'Abductors',
+      expanded: false,
+      imageUrl: '../../../assets/icon/abductors.jpg',
+      exercises: [
+        {
+          id: 'side-leg-lifts',
+          name: 'Side Leg Lifts',
+        },
+        {
+          id: 'hip-abduction-machine',
+          name: 'Hip Abduction Machine',
+        },
+        {
+          id: 'fire-hydrants',
+          name: 'Fire Hydrants',
+        },
+      ],
+    },
+    {
+      id: 'calves',
+      name: 'Calves',
+      expanded: false,
+      imageUrl: '../../../assets/icon/calves.jpg',
+      exercises: [
+        {
+          id: 'calf-raises',
+          name: 'Calf Raises',
+        },
+        {
+          id: 'seated-calf-raises',
+          name: 'Seated Calf Raises',
+        },
+        {
+          id: 'standing-calf-raises',
+          name: 'Standing Calf Raises',
+        },
+      ],
+    }
   ];
--- Invictus/src/app/pages/exercises/exercises.page.ts
+++ Invictus/src/app/pages/exercises/exercises.page.ts
@@ -76,10 +76,186 @@ export class ExercisesPage {
         },
       ],
     },
-    
-    
-
-    // Add more categories and exercises
+    {
+      id: 'back',
+      name: 'Back',
+      expanded: false,
+      imageUrl: '../../../assets/icon/back.jpg',
+      exercises: [
+        {
+          id: 'pull-up',
+          name: 'Pull-Up',
+        },
+        {
+          id: 'deadlift',
+          name: 'Deadlift',
+        },
+        {
+          id: 'bent-over-row',
+          name: 'Bent Over Row',
+        },
+      ],
+    },
+    {
+      id: 'legs',
+      name: 'Legs',
+      expanded: false,
+      imageUrl: '../../../assets/icon/legs.jpg',
+      exercises: [
+        {
+          id: 'squat',
+          name: 'Squat',
+        },
+        {
+          id: 'leg-press',
+          name: 'Leg Press',
+        },
+        {
+          id: 'lunges',
+          name: 'Lunges',
+        },
+      ],
+    },
+    {
+      id: 'triceps',
+      name: 'Triceps',
+      expanded: false,
+      imageUrl: '../../../assets/icon/triceps.jpg',
+      exercises: [
+        {
+          id: 'tricep-dips',
+          name: 'Tricep Dips',
+        },
+        {
+          id: 'tricep-extension',
+          name: 'Tricep Extension',
+        },
+        {
+          id: 'close-grip-bench-press',
+          name: 'Close Grip Bench Press',
+        },
+      ],
+    },
+    {
+      id: 'core',
+      name: 'Core',
+      expanded: false,
+      imageUrl: '../../../assets/icon/core.jpg',
+      exercises: [
+        {
+          id: 'plank',
+          name: 'Plank',
+        },
+        {
+          id: 'crunches',
+          name: 'Crunches',
+        },
+        {
+          id: 'leg-raises',
+          name: 'Leg Raises',
+        },
+      ],
+    },
+    {
+      id: 'cardio',
+      name: 'Cardio',
+      expanded: false,
+      imageUrl: '../../../assets/icon/cardio.jpg',
+      exercises: [
+        {
+          id: 'running',
+          name: 'Running',
+        },
+        {
+          id: 'jump-rope',
+          name: 'Jump Rope',
+        },
+        {
+          id: 'cycling',
+          name: 'Cycling',
+        },
+      ],
+    },
+    {
+      id: 'forearms',
+      name: 'Forearms',
+      expanded: false,
+      imageUrl: '../../../assets/icon/forearms.jpg',
+      exercises: [
+        {
+          id: 'wrist-curl',
+          name: 'Wrist Curl',
+        },
+        {
+          id: 'reverse-wrist-curl',
+          name: 'Reverse Wrist Curl',
+        },
+        {
+          id: 'hammer-grip-curl',
+          name: 'Hammer Grip Curl',
+        },
+      ],
+    },
+    {
+      id: 'abdominals',
+      name: 'Abdominals',
+      expanded: false,
+      imageUrl: '../../../assets/icon/abdominals.jpg',
+      exercises: [
+        {
+          id: 'crunches',
+          name: 'Crunches',
+        },
+        {
+          id: 'leg-raises',
+          name: 'Leg Raises',
+        },
+        {
+          id: 'planks',
+          name: 'Planks',
+        },
+      ],
+    },
+    {
+      id: 'abductors',
+      name: 'Abductors',
+      expanded: false,
+      imageUrl: '../../../assets/icon/abductors.jpg',
+      exercises: [
+        {
+          id: 'side-leg-lifts',
+          name: 'Side Leg Lifts',
+        },
+        {
+          id: 'hip-abduction-machine',
+          name: 'Hip Abduction Machine',
+        },
+        {
+          id: 'fire-hydrants',
+          name: 'Fire Hydrants',
+        },
+      ],
+    },
+    {
+      id: 'calves',
+      name: 'Calves',
+      expanded: false,
+      imageUrl: '../../../assets/icon/calves.jpg',
+      exercises: [
+        {
+          id: 'calf-raises',
+          name: 'Calf Raises',
+        },
+        {
+          id: 'seated-calf-raises',
+          name: 'Seated Calf Raises',
+        },
+        {
+          id: 'standing-calf-raises',
+          name: 'Standing Calf Raises',
+        },
+      ],
+    }
 ];
   constructor(private router: Router, public auth: AuthService) {}
   logout() {
