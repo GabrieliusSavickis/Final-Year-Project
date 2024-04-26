f0d2a0df8e275984c75e6c9451d63d57462546cc
Gabrielius <gabrrielius@gmail.com>
2024-04-26 10:10:42 +0100

    Added comments for login page

--- Invictus/src/app/pages/login/login.page.ts
+++ Invictus/src/app/pages/login/login.page.ts
@@ -2,15 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
-  adminUsername: string = '';
-  adminPassword: string = '';
+  adminUsername: string = ''; // Variable to store the admin username input
+  adminPassword: string = ''; // Variable to store the admin password input
 
   constructor(public auth: AuthService, private router: Router) { }
 
@@ -24,14 +23,14 @@ export class LoginPage {
     });
   }
 
-
+  // Function to handle admin login
   adminLogin() {
     if (this.adminUsername === 'admin' && this.adminPassword === 'admin123') {
+      // If admin credentials are valid, navigate to the admin page
       this.router.navigate(['/admin']);
     } else {
+      // If admin credentials are invalid, show an alert
       alert('Invalid credentials');
     }
   }
-
-
 }
